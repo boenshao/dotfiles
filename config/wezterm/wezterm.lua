b18f0349ef691bd8605d714ae458a0f3345e89cf
boenshao <boenshao@gmail.com>
2025-08-08 22:55:54 +0800

    config(wezterm): update wezterm config

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,9 +1,15 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.window_decorations = "RESIZE"
+local is_darwin <const> = wezterm.target_triple:find("darwin") ~= nil
+local is_linux <const> = wezterm.target_triple:find("linux") ~= nil
+
+if is_linux then
+    config.window_decorations = "RESIZE"
+end
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = 'NeverPrompt'
+config.adjust_window_size_when_changing_font_size = false
 
 config.window_padding = {
   left = 12,
