dc41188cfd4ea07228e82106e52747514d78b083
boenshao <boenshao@gmail.com>
2025-08-26 18:15:37 +0800

    feat: install user bin with dra

--- apps.sh
+++ apps.sh
@@ -5,16 +5,39 @@ set -euo pipefail
 # prepare
 sudo apt update && sudo apt install apt-transport-https curl wget gpg ca-certificates
 
-# utilities
+# basics
 sudo apt update && sudo apt install jq tmux cmake hexedit htop
-mkdir "$HOME/.local/bin"
+# platform sepcific
+# sudo apt install gnome-tweaks
+# sudo apt install powertop
+# sudo apt install fprintd libpam-fprintd
+# sudo apt install intel-media-va-driver-non-free
+# sudo apt install intel-gpu-tools
+
+mkdir -p "$HOME/.local/bin"
+# cli, install with dra
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/devmatteini/dra/refs/heads/main/install.sh | bash -s -- --to ~/.local/bin
+dra download -i -s "bat-v{tag}-x86_64-unknown-linux-gnu.tar.gz" -o ~/.local/bin/ sharkdp/bat
+dra download -i -s "delta-{tag}-x86_64-unknown-linux-gnu.tar.gz" -o ~/.local/bin/ dandavison/delta
+dra download -i -s "eza_x86_64-unknown-linux-gnu.tar.gz" -o ~/.local/bin/ eza-community/eza
+dra download -i -s "fd-v{tag}-x86_64-unknown-linux-gnu.tar.gz" -o ~/.local/bin/ sharkdp/fd
+dra download -i -s "hexyl-v{tag}-x86_64-unknown-linux-gnu.tar.gz" -o ~/.local/bin/ sharkdp/hexyl
+dra download -i -s "ripgrep-{tag}-x86_64-unknown-linux-musl.tar.gz" -o ~/.local/bin/ BurntSushi/ripgrep
+dra download -i -s "sops-v{tag}.linux.amd64" -o ~/.local/bin/ getsops/sops
+# vim
+dra download -i -s "nvim-linux-x86_64.appimage" -o ~/.local/bin/ neovim/neovim
 # fzf, https://github.com/junegunn/fzf#installation
 git clone --depth 1 https://github.com/junegunn/fzf.git "${XDG_DATA_HOME:-$HOME/.local/share}/fzf"
 "${XDG_DATA_HOME:-$HOME/.local/share}/fzf/install" --xdg --no-bash --no-fish
-# bat delta eza fd fq hexyl presenterm rg sops
-# kubectl helm
-# nvim
-# bun uv rustup go
+
+# toolchain
+# bun
+curl -fsSL https://bun.com/install | bash
+# uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+# rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# kubectl helm go
 
 # docker, https://docs.docker.com/engine/install/ubuntu/
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -39,7 +62,6 @@ sudo apt update && sudo apt install terraform
 
 # code, https://code.visualstudio.com/docs/setup/linux
 
-
 # flatpak
 flatpak install flathub io.github.ungoogled_software.ungoogled_chromium
 flatpak install flathub com.brave.Browser
@@ -49,10 +71,3 @@ flatpak install flathub org.videolan.VLC
 flatpak install flathub me.iepure.devtoolbox
 flatpak install flathub com.usebruno.Bruno
 flatpak install flathub it.mijorus.gearlever
-
-# platform sepcific
-# sudo apt install gnome-tweaks
-# sudo apt install powertop
-# sudo apt install fprintd libpam-fprintd
-# sudo apt install intel-media-va-driver-non-free
-# sudo apt install intel-gpu-tools
